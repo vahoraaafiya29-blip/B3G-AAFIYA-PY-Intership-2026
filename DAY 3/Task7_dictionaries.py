1ad906cc5e3c6f4f286600ecd844bf422f464b00
vahoraaafiya29-blip <vahoraaafiya29@gmail.com>
2026-06-25 19:28:18 +0530

    Aafiya

--- DAY 3/Task7_dictionaries.py
+++ DAY 3/Task7_dictionaries.py
@@ -1,53 +1,21 @@
-
-# Inventory System
-
 inventory = {
-    "Pen": 20,
-    "Notebook": 15,
-    "Pencil": 30
+    "Pen": 100,
+    "Notebook": 50
 }
 
-while True:
-    print("\n--- INVENTORY SYSTEM ---")
-    print("1. Add new stock")
-    print("2. Sell product")
-    print("3. Show inventory")
-    print("4. Exit")
+inventory["Pen"]=inventory["Pen"] + 10
+product="Notebook"
 
-    choice = input("Enter your choice: ")
+if"Notebook"in inventory:
+    inventory["Notebook"]=inventory["Notebook"]-1
+    print("Notebook sold")
+else:
+    print("Notebook Not Found")
+    product="Bag"
 
-    if choice == "1":
-        product = input("Enter product name: ")
-        quantity = int(input("Enter quantity to add: "))
-
-        if product in inventory:
-            inventory[product] += quantity
-        else:
-            inventory[product] = quantity
-
-        print("Stock added successfully.")
-
-    elif choice == "2":
-        product = input("Enter product name to sell: ")
-        quantity = int(input("Enter quantity to sell: "))
-
-        if product in inventory:
-            if inventory[product] >= quantity:
-                inventory[product] -= quantity
-                print("Product sold successfully.")
-            else:
-                print("Not enough stock available.")
-        else:
-            print("Product does not exist in inventory.")
-
-    elif choice == "3":
-        print("\nCurrent Inventory:")
-        for product, quantity in inventory.items():
-            print(product, ":", quantity)
-
-    elif choice == "4":
-        print("Thank you!")
-        break
-
+    if "Bag" in inventory:
+        inventory["Bag"]=inventory["Bag"]-1
     else:
-        print("Invalid choice. Please try again.")
+        print("Bag Not Found")
+        print("Updated Inventory:")
+        print(inventory)
